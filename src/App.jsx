dfaeb36e5b2b93089db40b0a234c47506c1aa717
5424076ktbya <chya24076@g.nihon-u.ac.jp>
2026-05-21 13:11:02 +0900

    宿題

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+export default function App() {
+  const width = 400;
+  const height = 400;
+  const rect = [
+    {x: 100, y: 100, w: 250, h: 30, color: 'orange', label: 'A'},
+    {x: 100, y: 200, w: 200, h: 30, color: 'purple', label: 'B'},
+    {x: 100, y: 300, w: 100, h: 30, color: 'pink', label: 'C'}
+  ];
+
+  return (
+    <svg width={width} height={height}>
+        <line x1="100" y1="0" x2="100" y2="400" stroke="black" />
+
+        {rect.map(({x,y,w, h, color,label}, i) => (
+          <g key={i}>
+            <text x={x-15} y={y+5} textAnchor="end" >
+              {label}
+            </text>
+
+            <line x1="90" y1={y} x2="100" y2={y} stroke="black" />
+
+            <rect
+                x={x}
+                y={y-h/2}
+                width={w}
+                height={h}
+                fill={color}
+            />
+          </g>
+        ))}
+    </svg>
+  );
+};
